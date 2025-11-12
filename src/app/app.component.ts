8a770ec025357efe7aa06c53a4b8515732459ccb
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-13 00:48:00 +0100

    added headerComponent and styled it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BackgroundComponent } from './background/background.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BackgroundComponent],
+  imports: [CommonModule, RouterOutlet, BackgroundComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
