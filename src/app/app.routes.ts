83ae72474a5a24d2411c51e8cfb6b36cfcdbaa20
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-13 17:19:24 +0100

    added LegalNotice and adjusted header position

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'legal-notice', component: LegalNoticeComponent },
+];
+
+
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule], // ✅ Add this line
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent {}
 
-}
