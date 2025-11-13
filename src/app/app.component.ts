5e1c017db5ba310d8dc4184a343734fb960ab686
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-13 16:04:54 +0100

    added Footer and styled it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BackgroundComponent } from './background/background.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BackgroundComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, BackgroundComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
