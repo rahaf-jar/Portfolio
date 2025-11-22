6e18dcb99af73cd3b0329e290cd4514b3a6bbf2b
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-22 16:12:20 +0100

    added about me component (still working on it)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
