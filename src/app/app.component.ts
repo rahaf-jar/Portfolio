90bf6b81f62215395418be1cc0b4dad04cb23846
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-23 20:36:29 +0100

    added portfolio component and made padding class global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroComponent, AboutMeComponent, MySkillsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
