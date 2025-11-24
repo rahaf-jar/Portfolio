b8a411f73a681d6d734dbec41f421676f11ddc8f
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-24 16:11:18 +0100

    added contact componant and edited portfolio header style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HeroComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
