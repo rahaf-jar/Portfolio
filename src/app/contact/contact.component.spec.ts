a05bd05d1ee1891fcee647f7a6a73f745db6a0fb
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-29 17:39:51 +0100

    added simple input validation

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import { NgModel } from '@angular/forms';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,7 +9,7 @@ describe('ContactComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContactComponent]
+      imports: [ContactComponent, NgModel]
     })
     .compileComponents();
     
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  name = '';
+  email = '';
+  message = '';
+  checkbox = false;
 
+  nameError = false;
+  emailError = false;
+  messageError = false;
+  checkboxError = false;
+
+  validateName() {
+    this.nameError = this.name.trim() === '';
+  }
+
+  validateEmail() {
+    this.emailError = this.email.trim() === '';
+  }
+
+  validateMessage() {
+    this.messageError = this.message.trim() === '';
+  }
+
+  validateCheckbox() {
+    this.checkboxError = this.checkbox;
+  }
 }
