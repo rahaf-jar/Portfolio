9efdbc2efa06eeb3727d4528e3f9441bb0b28417
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-30 15:37:58 +0100

    added footer navigation to contact

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ScrollService } from '../scroll.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +11,14 @@ import { ScrollService } from '../scroll.service';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  constructor(public scroll: ScrollService){}
+  constructor(public scroll: ScrollService, private router: Router){}
+
+  navigateToSection(section: string) {
+    this.router.navigate(['/']).then(() => {
+      setTimeout(() => {
+        this.scroll.scrollTo(section);
+      }, 50);
+    });
+  }
 }
 
