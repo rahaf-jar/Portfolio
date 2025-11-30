e5fcc582834207eead27b41d696a2223b510f486
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-30 23:42:02 +0100

    added about_me translation

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent {
-
+  constructor(public translation: TranslationService) {}
 }
