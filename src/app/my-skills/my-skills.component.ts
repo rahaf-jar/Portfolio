cc4528ca8c1aead7087865b2f52bebd25c7b4a3a
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-30 23:56:57 +0100

    added skills translation

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { NgFor, NgClass } from '@angular/common';
 import { ScrollService } from '../services/scroll.service';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
   imports: [NgFor, NgClass],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
 
-  constructor(public scroll: ScrollService){}
+  constructor(public scroll: ScrollService, public translation: TranslationService){}
 
   skills = [
     { name: 'HTML', icon: 'html.svg' },
@@ -27,5 +28,3 @@ export class MySkillsComponent {
     { name: 'Continually Learning', icon: 'continually-learning.svg' }
   ];
 }
-
-
