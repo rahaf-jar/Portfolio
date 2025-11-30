7902576df3bf7179c1e71c2f8a1ceaa4cc9ea1f5
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-11-30 15:30:26 +0100

    took out unnecessary imports

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
