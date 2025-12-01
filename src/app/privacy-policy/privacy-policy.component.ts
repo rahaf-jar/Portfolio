552f7e5120ec9bc531c28edddcc5d89aae4d79dc
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-12-01 15:25:28 +0100

    added privacy policy translation

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ScrollService } from '../services/scroll.service';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-  constructor(public scroll: ScrollService, private router: Router){}
+
+  constructor(
+    public scroll: ScrollService, 
+    private router: Router, 
+    public translation: TranslationService
+  ) {}
 
   navigateToSection(section: string) {
     this.router.navigate(['/']).then(() => {
@@ -19,5 +24,4 @@ export class PrivacyPolicyComponent {
       }, 50);
     });
   }
-
 }
