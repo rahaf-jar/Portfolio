016f14c93a34101410ea1d9edacc7b91432095bd
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2026-01-06 21:02:49 +0100

    added animation using scss and AOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AfterViewInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,15 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+
+  ngAfterViewInit(): void {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+
+    AOS.refresh();
+  }
 }
